44cc9c96cc0795860c95516d73c1c383ae1a8544
Anthony9219 <318192@my.puhsd.org>
2021-10-20 17:21:40 -0700

    made changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/c3.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Disabled
+@Autonomous (name = "c3")
+public class c3 extends LinearOpMode {
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+    DcMotor motor5;
+    Servo servo1;
+    DcMotor motor6;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        motor5 = hardwareMap.dcMotor.get("motor5");
+
+        waitForStart();
+        //Strafe Right
+        frontLeft.setPower(.3);
+        frontRight.setPower(-.3);
+        backLeft.setPower(-.3);
+        backRight.setPower(.3);
+        sleep(1000);
+
+        //motor 5 spin
+        motor5.setPower(.5);
+        sleep(20000);
+
+        //Strafe Left
+        frontLeft.setPower(-.3);
+        frontRight.setPower(.3);
+        backLeft.setPower(.3);
+        backRight.setPower(-.3);
+        sleep(2000);
+
+        //Forward
+        frontLeft.setPower(.2);
+        frontLeft.setPower(-.2);
+        backLeft.setPower(.2);
+        backRight.setPower(-.2);
+        sleep(500);
+
+        //Grab object
+        servo1.setPosition(50);
+
+        //Lift Object
+        motor6.setPower(.5);
+        sleep(600);
+
+
+
+
+
+    }
+}

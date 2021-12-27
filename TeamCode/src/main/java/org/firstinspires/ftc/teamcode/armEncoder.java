162bb30602488d1b6e3f1b0426eb90fa55cc2f5d
GenesisJM <309017@my.puhsd.org>
2021-12-27 13:58:21 -0800

    added directions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armEncoder.java
@@ -27,7 +27,7 @@ public class armEncoder extends LinearOpMode {
     static final double     WHEEL_DIAMETER_INCHES   = 3.77953;     // For figuring circumference
 
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.1;
+    static final double     DRIVE_SPEED             = 0.2;
 
 
     //values for arm
@@ -45,49 +45,49 @@ public class armEncoder extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-       // frontLeft = hardwareMap.dcMotor.get("frontLeft");
-        //frontRight = hardwareMap.dcMotor.get("frontRight");
-        // backLeft = hardwareMap.dcMotor.get("backLeft");
-        //backRight = hardwareMap.dcMotor.get("backRight");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
 
-        //carousel = hardwareMap.dcMotor.get("carousel");
+        carousel = hardwareMap.dcMotor.get("carousel");
         arm = hardwareMap.dcMotor.get("arm");
-        // arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        //claw = hardwareMap.servo.get("claw");
+        claw = hardwareMap.servo.get("claw");
 
-        //frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        //backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Status", "Resetting Encoders");    //
         telemetry.update();
 
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        // backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Send telemetry message to indicate successful Encoder reset
         telemetry.addData("Path0",  "Starting at %7d ",
-                arm.getCurrentPosition());
-        // frontRight.getCurrentPosition(),
-        // backLeft.getCurrentPosition(),
-        // backRight.getCurrentPosition())
-        telemetry.update();
+                arm.getCurrentPosition(),
+         frontRight.getCurrentPosition(),
+         backLeft.getCurrentPosition(),
+         backRight.getCurrentPosition());
+         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -98,8 +98,13 @@ public class armEncoder extends LinearOpMode {
         //Go Forward
         telemetry.addData("Running", "Encoder Test...");
         telemetry.update();
-        armEncoderDrive(DRIVE_SPEED,  LVL_2_INCHES,  30.0);
-        armEncoderDrive(DRIVE_SPEED,  -LVL_2_INCHES,  30.0);
+        claw.setPosition(0);
+        sleep(250);
+        armEncoderDrive(DRIVE_SPEED,  LVL_3_INCHES,  30.0);
+        encoderDrive(DRIVE_SPEED,2,2,5.0);
+        claw.setPosition(.3);
+        sleep(250);
+       // armEncoderDrive(.1,  -5,  30.0);
 
         //Go Backward
         //Turn Right
@@ -189,6 +194,77 @@ public class armEncoder extends LinearOpMode {
             sleep(250);   // optional pause after each move
         }
     }
+    public void encoderDrive(double speed, double leftInches, double rightInches, double timeoutS) {
+        int newFrontLeftTarget;
+        int newBackLeftTarget;
+        int newFrontRightTarget;
+        int newBackRightTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            // Determine new target position, and pass to motor controller
+            newFrontLeftTarget = frontLeft.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newBackLeftTarget = backLeft.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newFrontRightTarget = frontRight.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            newBackRightTarget = backRight.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            frontLeft.setTargetPosition(newFrontLeftTarget);
+            backLeft.setTargetPosition(newBackLeftTarget);
+            frontRight.setTargetPosition(newFrontRightTarget);
+            backRight.setTargetPosition(newBackRightTarget);
+            // Turn On RUN_TO_POSITION
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            // reset the timeout time and start motion.
+            runtime.reset();
+            frontLeft.setPower(Math.abs(speed));
+            backLeft.setPower(Math.abs(speed));
+            frontRight.setPower(Math.abs(speed));
+            backRight.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (frontLeft.isBusy() && frontRight.isBusy() && backLeft.isBusy() && backRight.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d : %7d: %7d ", newFrontLeftTarget, newBackLeftTarget, newFrontRightTarget, newBackRightTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d : %7d: %7d ",
+                        frontLeft.getCurrentPosition(),
+                        backLeft.getCurrentPosition(),
+                        frontRight.getCurrentPosition(),
+                        backRight.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            frontLeft.setPower(0);
+            backLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            sleep(250);   // optional pause after each move
+        }
+    }
+
     public void encoderDriveStrafe(double speed,
                                    double leftInches, double rightInches,
                                    double timeoutS) {

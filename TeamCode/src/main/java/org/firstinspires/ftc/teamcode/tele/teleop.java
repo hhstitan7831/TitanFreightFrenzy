c8102405bc0c99db09e4001cceb0f2dce6bb5dcf
Anthony9219 <318192@my.puhsd.org>
2021-12-29 12:21:20 -0800

    new arm encoders for new tornado motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/teleop.java
@@ -46,7 +46,7 @@ public class teleop extends OpMode {
 
         //Game Related (P2)
         if (Math.abs(gamepad2.left_trigger) > .1) {
-            robot.arm.setPower(gamepad2.left_trigger * .8);
+            robot.arm.setPower(gamepad2.left_trigger );
         } else if (Math.abs(gamepad2.right_trigger) > .1){
             robot.arm.setPower(-gamepad2.right_trigger);
         } else {
